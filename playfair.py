6a320fca35c749361aeb1dae2a747e33518e93ac
Robert Makatura <rmakatura10@stuy.edu>
2020-10-11 15:44:37 -0400

    finished with the decode\

--- playfair.py
+++ playfair.py
@@ -1,8 +1,3 @@
-table = []
-stringtable = ""
-tls = []
-
-
 def encode(m, s):
     table = []
     stringtable = ""
@@ -57,6 +52,64 @@ def encode(m, s):
                 tls[i][0] = table[r0w][colum1]
                 tls[i][1] = table[r1w][colum0]
                 print(tls[i], "step 3", table[r0w][colum1], table[r1w][colum0], "\n")
+    finalstwing = ""
+    for i in range(len(tls)):
+        finalstwing += ''.join(tls[i])
+        print('--' + finalstwing)
+    return(finalstwing)
+def decode(m, s):
+    table = []
+    stringtable = ""
+    tls = []
+    stringtable = s
+    for i in range(5):
+        table.append([])
+        for e in range(5):
+            table[i].append(stringtable[e + i * 5])
+    i = 1
+    try:
+        while(i < len(m)):
+            tls.append([m[i-1],m[i]])
+            i+=2
+    except:
+        return("your string must be an even number of characters")
+    table.append(table[0])
+    print(table)
+    for i in range(len(tls)):
+        colum0 = -500
+        colum1 = 500
+        r0w = -500
+        r1w = 500
+        boolean = True
+        for row in range(5):
+            if table[row].count(tls[i][0]) == 1 and table[row].count(tls[i][1]) == 1 and boolean:
+                print('rows same')
+                tls[i][0] = table[row - 1][table[row].index(tls[i][0])]
+                tls[i][1] = table[row - 1][table[row].index(tls[i][1])]
+                print(tls[i])
+                boolean = False
+            else:
+                for column in range(5):
+                    if table[row][column] == tls[i][0]:
+                        colum0 = column
+                        r0w = row
+                        print(tls[i][0], ' ', r0w, ' ', colum0)
+                    if table[row][column] == tls[i][1]:
+                        colum1 = column
+                        r1w = row
+                        print(tls[i][1], ' ', r1w, ' ', colum1)
+# check this part because it's in the for statement make sure it makes sense
+                    if colum0 == colum1 and boolean:
+                        print('columns same ', tls[i], 'column0, 1 == ', colum0, ' ', colum1)
+                        boolean = False
+                        tls[i][0] = table[r0w][(colum0 - 1)]
+                        tls[i][1] = table[r1w][(colum1 - 1)]
+                        print(tls[i])
+        if colum1 != colum0 and boolean:
+            print(colum1, " and ", colum0)
+            tls[i][0] = table[r0w][colum1]
+            tls[i][1] = table[r1w][colum0]
+            print(tls[i], "step 3", table[r0w][colum1], table[r1w][colum0], "\n")
     finalstwing = ""
     for i in range(len(tls)):
         finalstwing += ''.join(tls[i])
